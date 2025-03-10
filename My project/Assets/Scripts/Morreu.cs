db76c852bcdc3ae15c4d526d6175cbf217b0cae8
protons77 <souza.diego@escolar.ifrn.edu.br>
2025-03-10 16:12:48 -0300

    jogoGG

--- My project/Assets/Scripts/Morreu.cs
+++ My project/Assets/Scripts/Morreu.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Moreu : MonoBehaviour
 {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
